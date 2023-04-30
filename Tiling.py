5e72271491e17028f6b3ca850a2c17cf026e1b3a
Stoicheia <37011850+Stoicheia@users.noreply.github.com>
2023-04-30 18:00:42 +1000

    v

--- Tiling.py
+++ Tiling.py
@@ -5,6 +5,8 @@ Created on Sun Apr 30 16:42:56 2023
 @author: pieis
 """
 
+import numpy as np
+
 class Tiling:
     def __init__(self, IFS, attractor, theta):
         # IFS a list of [IFSfunc]. IFSfunc has scaling, apply and applyinv
@@ -17,22 +19,49 @@ class Tiling:
         self.theta = theta
 
         self.thetaLength = len(theta)
+        self.sigma = len(IFS)
 
         # List of theta sums
         self.sumTheta = []
         self.iterations = []
         for i in range(len(theta)):
-            self.sumTheta.append(sum(theta[0:i+1]))
+            s = sum([self.IFS[k].scaling for k in theta[0:i]])
+            self.sumTheta.append(s)
             
     def getIteration(self, k):
         # if iteration stored, read, else calculateIteration(k)
         pass
             
     def calculateIteration(self, k):
-        pass
+        polygons = []
+        addresses = self.omegaK(k)
+        bigPolyVertices = self.attractor.exterior.coords.xy
+        for sigma in addresses:
+            projectionMatrix = np.eye(3)
+            for i in sigma:
+                projectionMatrix = np.matmul(projectionMatrix, self.IFS[i].matrix)
+            for j in self.theta[0:k]:
+                projectionMatrix = np.matmul(self.IFS[j].matrixInverse, projectionMatrix)
+            vertices = []
+            
+            
     
     def omegaK(self, k):
-        # lexicographically ordered indices 
-        pass
+        return self.omegaKPartial([], self.sumTheta[k])
+    
+    def omegaKPartial(self, root, target):
+        result = []
+        rootSum = sum(self.IFS[k].scaling for k in root)
+        for i in range(self.sigma):
+            rootPlusOne = root
+            rootPlusOne.append(i)
+            newSum = rootSum + self.IFS[i].scaling
+            if newSum > target:
+                result += [rootPlusOne]
+            else:
+                result += self.omegaKPartial(rootPlusOne, target)
+        return result
+    
+    
     
     
