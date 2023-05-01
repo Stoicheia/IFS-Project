b8bf336dcb7a0bf3b9bce8b015d1ca757dd51108
Charlie <u6661060@anu.edu.au>
2023-05-01 20:51:23 +1000

    In progress - additions to the application to plot

--- Application.py
+++ Application.py
@@ -1,17 +1,18 @@
-
-import matplotlib
-import tkinter
-import shapely
-
 import tkinter as tk
+# import shapely
 from tkinter import filedialog
 import json
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 from IFSLibrary import *
+from Tiling import Tiling
 
 FONT = ("Helvetica", 12)
+
+def colourMap(n, name = 'hsv'):
+    return plt.cm.get_cmap(name, n)
+
 
 class Application:
     def __init__(self, IFS = [], attractor = [], theta = []):
@@ -20,6 +21,8 @@ class Application:
         self.theta = theta
 
         self.iteration = 1
+
+        self.tiling = Tiling(IFS, attractor, theta)
         
         self.root = tk.Tk()
         self.root.title("Graph IFS Plotter")
@@ -61,10 +64,10 @@ class Application:
         
     def validate_input(self):
         value = self.input_entry.get()
-        if value.isdigit() and 1 <= int(value) <= self.K and len(value) <= self.iteration+1:
+        if value.isdigit() and 1 <= int(value) <= len(self.theta) and len(value) <= self.iteration+1:
             return True
         else:
-            self.error_label.config(text="Input must be an integer between 1 and {} with at most {} digits".format(self.K, self.iteration+1))
+            self.error_label.config(text="Input must be an integer between 1 and {} with at most {} digits".format(len(self.theta), self.iteration+1))
             self.root.after(2000, self.clear_error)
             return False
         
@@ -77,7 +80,7 @@ class Application:
             self.plot_iteration()
         
     def next_iteration(self):
-        if self.iteration < self.K:
+        if self.iteration < len(self.theta):
             if self.validate_input():
                 self.iteration += 1
                 self.plot_iteration()
@@ -86,6 +89,13 @@ class Application:
     def plot_iteration(self, iteration):
         self.ax.clear()
         self.ax.set_title("Iteration {}".format(self.iteration))
+
+        # Plot each of the polygons provided in the 
+        polys = self.tiling.getIteration(iteration)
+        colours = colourMap(len(polys))
+        for i, poly in enumerate(polys):
+            pltPoly = plt.Polygon(poly.exterior.xy, facecolor = colours[i], alpha = 0.5)
+            self.ax.add_path(pltPoly)
 
         self.canvas.draw()
         
@@ -109,21 +119,18 @@ class IFSInput:
         # Create input labels and fields
         rotation_label = tk.Label(self.frame, text="Rotation")
         rotation_label.grid(row=1, column=0)
-        rotation_entry = tk.Entry(self.frame)
-        rotation_entry.grid(row=1, column=1)
-        self.entries.append(rotation_entry)
+        self.rotation = tk.Entry(self.frame)
+        self.rotation.grid(row=1, column=1)
         
         scaling_label = tk.Label(self.frame, text="Scaling")
         scaling_label.grid(row=2, column=0)
-        scaling_entry = tk.Entry(self.frame)
-        scaling_entry.grid(row=2, column=1)
-        self.entries.append(scaling_entry)
+        self.scaling = tk.Entry(self.frame)
+        self.scaling.grid(row=2, column=1)
         
         translation_label = tk.Label(self.frame, text="Translation")
         translation_label.grid(row=3, column=0)
-        translation_entry = tk.Entry(self.frame)
-        translation_entry.grid(row=3, column=1)
-        self.entries.append(translation_entry)
+        self.translation = tk.Entry(self.frame)
+        self.translation.grid(row=3, column=1)
         
         # Create submit button
         submit_button = tk.Button(self.frame, text="Submit", command=self.submit)
@@ -134,7 +141,10 @@ class IFSInput:
         upload_button.grid(row=5, column=0, columnspan=2)
 
     def submit(self):
-        values = [entry.get() for entry in self.entries]
+        entries = [self.rotation, self.scaling, self.translation]
+        values = [entry.get()  for entry in entries]
+        for e in entries: # Clear the entries (TODO: only clear if successful entry)
+            e.delete(0, tk.END)
         print(values)
 
     def upload(self):
