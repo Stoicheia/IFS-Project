dcf23554c34723e165bb17f81e578b4f8dcc864e
Charlie <u6661060@anu.edu.au>
2023-05-02 20:06:39 +1000

    Application improvements

--- Application.py
+++ Application.py
@@ -34,33 +34,50 @@ class Application:
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
         self.canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
-        self.init_side_panel()
-        
-    def init_side_panel(self):
         # Create side panel
         self.panel = tk.Frame(self.root)
         self.panel.pack(side=tk.RIGHT, fill=tk.Y)
+        self.init_side_panel(self.panel)
         
-        # ADD BUTTONS
-        self.iteration_frame = tk.Frame(self.panel)
+    def init_side_panel(self, frame):
+        ###############################
+        # Iteration Control
+        self.iteration_frame = tk.Frame(frame)
         self.iteration_frame.pack(side = tk.TOP, fill = tk.X)
+        self.init_iteration_control(self.iteration_frame)
+        
+        ####################
+        # IFS Controlling
+        self.control_frame = tk.Frame(frame)
+        self.control_frame.pack(side = tk.TOP)
+        tk.Label(self.control_frame, text="Theta Input:").pack(side=tk.TOP, padx=10, pady=5)
+        self.input_theta = tk.Entry(self.control_frame)
+        self.input_theta.pack(side=tk.TOP, padx=10, pady=5)
 
+
+        # IFS Inputting
+        self.inputFrame = tk.Frame(frame)
+        self.inputFrame.pack(side = tk.BOTTOM)
+        self.ifs_input = IFSInput(self.inputFrame)
+        self.ifs_input.upload_button.config(command = lambda : self.updateIFS(self.ifs_input.upload()))
+
+    def init_iteration_control(self, frame):
         # Add previous button
-        prev_frame = tk.Frame(self.iteration_frame)
+        prev_frame = tk.Frame(frame)
         prev_frame.pack(side = tk.LEFT)
         tk.Label(prev_frame, text = "Previous iteration").pack(side=tk.TOP)
         self.prev_button = tk.Button(prev_frame, text="<<", command=self.prev_iteration)
         self.prev_button.pack(side=tk.BOTTOM, padx=10, pady=10)
         
-        # Add next
-        next_frame = tk.Frame(self.iteration_frame)
+        # Add next button
+        next_frame = tk.Frame(frame)
         next_frame.pack(side = tk.RIGHT)
         tk.Label(next_frame, text = "Next iteration").pack(side=tk.TOP)
         self.next_button = tk.Button(next_frame, text=">>", command=self.next_iteration)
         self.next_button.pack(side=tk.BOTTOM, padx=10, pady=10)
         
         # Add a jump to iteration button
-        jump_frame = tk.Frame(self.iteration_frame)
+        jump_frame = tk.Frame(frame)
         jump_frame.pack(side = tk.LEFT, fill = tk.X)
         tk.Label(jump_frame, text="Jump to Iteration:").pack(side = tk.TOP, padx=10, pady=5)
         self.iter_entry = tk.Entry(jump_frame)
@@ -72,18 +89,6 @@ class Application:
 
         self.jump_iter_button = tk.Button(jump_frame, text = "=>", command = iter_jump)
         self.jump_iter_button.pack(side = tk.BOTTOM)
-        
-        # IFS Controlling
-        self.control_frame = tk.Frame(self.panel)
-        self.control_frame.pack(side = tk.TOP)
-        self.controls = IFSControl(self.control_frame)
-
-
-        # IFS Inputting
-        self.inputFrame = tk.Frame(self.panel)
-        self.inputFrame.pack(side = tk.BOTTOM)
-        self.ifs_input = IFSInput(self.inputFrame)
-        self.ifs_input.upload_button.config(command = lambda : self.updateIFS(self.ifs_input.upload()))
 
     def updateIFS(self, ifs_parameters):
         IFS, A, theta = ifs_parameters
@@ -153,7 +158,6 @@ class IFSInput:
     def __init__(self, frame):
         self.frame = frame
         tk.Label(self.frame, text = "IFS Input", font = (FONT[0], 18)).grid(row = 0, column = 0, columnspan = 2)
-        self.entries = []
         
         # Create input labels and fields
         rotation_label = tk.Label(self.frame, text="Rotation")
@@ -190,24 +194,6 @@ class IFSInput:
         filename = filedialog.askopenfilename(initialdir=".", title="Select file", filetypes=(("JSON files", "*.json"),))
         if filename:
             return Parser.parse(filename)
-
-            # print(filename)
-            # with open(filename, "r") as f:
-            #     data = json.load(f)
-            #     self.entries[0].insert(0, str(data.get("rotation", "")))
-            #     self.entries[1].insert(0, str(data.get("scaling", "")))
-            #     self.entries[2].insert(0, str(data.get("translation", "")))
-
-class IFSControl:
-    def __init__(self, frame):
-        self.frame = frame
-        tk.Label(self.frame, text="Iteration:").pack(side=tk.TOP, padx=10, pady=5)
-        self.input_iteration = tk.Entry(self.frame)
-        self.input_iteration.pack(side=tk.TOP, padx=10, pady=5)
-
-        tk.Label(self.frame, text="Theta Input:").pack(side=tk.TOP, padx=10, pady=5)
-        self.input_theta = tk.Entry(self.frame)
-        self.input_theta.pack(side=tk.TOP, padx=10, pady=5)
 
 
 if __name__ == "__main__":
