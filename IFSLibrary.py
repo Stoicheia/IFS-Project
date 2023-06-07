3aa2371f82cc5f3754b4bd046ac2d10f2e0cea5e
Stoicheia <37011850+Stoicheia@users.noreply.github.com>
2023-06-07 21:07:35 +1000

    add to library

--- IFSLibrary.py
+++ IFSLibrary.py
@@ -40,3 +40,68 @@ class IFSfunc:
         vec = np.array([x,y,1])
         newVec = np.matmul(self.matrixInverse, vec)
         return (newVec[0], newVec[1])
+    
+class graphVertex:
+    def __init__(self, polygon):
+        self.polygon = polygon
+        self.outgoing = []
+        self.ingoing = []
+        
+    def addEdgeOutgoing(self, edgeIndex):
+        self.outgoing.append(edgeIndex)
+            
+    def addEdgeIngoing(self, edgeIndex):
+        self.ingoing.append(edgeIndex)
+
+class graphEdge:
+    def __init__(self, fromIndex, toIndex, func):
+        self.fromIndex = fromIndex
+        self.toIndex = toIndex
+        self.func = func
+        
+class IFSgraph:
+    
+    def __init__(self, vertices, edges):
+        self.vertices = vertices
+        self.edges = edges
+        for i in range(len(self.edges)):
+            edge = self.edges[i]
+            self.vertices[edge.fromIndex].addEdgeOutgoing(i)
+            self.vertices[edge.fromIndex].addEdgeIngoing(i)
+        self.sigma = len(edges)
+
+        
+    def getAdjacentEdges(self, vertexIndex):
+        return self.vertices[vertexIndex].outgoing
+    
+    def getFunc(self, index):
+        return self.edges[index].func
+    
+    def getPoly(self, vIndex):
+        return self.vertices[vIndex].polygon
+            
+    # gives list of all valid addresses
+    def omegaK(self, k, theta):
+        thetaSum = sum([self.getFunc(j).scaling for j in theta[0:k]])
+        allPaths = []
+        if k > 0:
+            allPaths.append(self.omegaKPartial([], thetaSum, self.edges[theta[-1]].toIndex))
+            return allPaths
+        elif k == 0:
+            return []
+    
+    def omegaKPartial(self, root, target, vIndex):
+        result = []
+        rootSum = sum(self.getFunc(k - 1).scaling for k in root)
+        if(rootSum > target):
+            return []
+        for i in self.vertices[vIndex].ingoing: # IFS = [f1, f2, f3] -> range(1, 4) = (1, 2, 3)
+            rootPlusOne = root.copy()
+            rootPlusOne.append(i)
+            newSum = rootSum + self.getFunc(i - 1).scaling
+            if newSum >= target:
+                result += [rootPlusOne]
+            else:
+                result += self.omegaKPartial(rootPlusOne, target, self.edges[i - 1].fromIndex)
+        return result
+        
