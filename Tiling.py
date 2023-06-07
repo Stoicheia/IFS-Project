bb4018d8fd4a0b1c382b54857f3fbea8a07a2365
Charlie <u6661060@anu.edu.au>
2023-06-07 23:18:05 +1000

    Reversing the orders

--- Tiling.py
+++ Tiling.py
@@ -51,9 +51,15 @@ class Tiling:
             initialPoly = self.IFSgraph.getPoly(self.IFSgraph.edges[sigma[-1]].toIndex)
             bigPolyVertices = initialPoly.exterior.coords
             projectionMatrix = np.eye(3)
+            s = sigma.copy()
+            s.reverse()
+            print("s",s)
             for i in sigma:
                 projectionMatrix = np.matmul(self.IFSgraph.edges[i].func.matrix, projectionMatrix)
-            for j in self.theta[0:k]:
+            thetak = self.theta[0:k]
+            thetak.reverse()
+            print("th", thetak)
+            for j in thetak:
                 projectionMatrix = np.matmul(self.IFSgraph.edges[j].func.matrixInverse, projectionMatrix)
             vertices = []
             for vertex in bigPolyVertices:
